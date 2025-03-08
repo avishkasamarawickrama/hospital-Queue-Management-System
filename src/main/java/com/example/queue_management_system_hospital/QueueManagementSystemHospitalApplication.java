f69c11dffb030aeadbd30e7f3f01dae320c0f90c
avishka <avishkasamarawickrama@gmail.com>
2025-03-08 13:39:09 +0530

    Project Created.

---
+++ src/main/java/com/example/queue_management_system_hospital/QueueManagementSystemHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.queue_management_system_hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueManagementSystemHospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueManagementSystemHospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/queue_management_system_hospital/QueueManagementSystemHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.queue_management_system_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueManagementSystemHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
